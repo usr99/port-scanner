b39494ba3a211e99c23e06c26eed04593b62f4d7
mamartin <mamartin@student.42quebec.com>
2023-01-25 13:16:45 -0500

    probe builder tests

--- src/cli.rs
+++ src/cli.rs
@@ -23,7 +23,7 @@ pub struct Args {
 
 	/// File that contains ip addresses to scan
 	#[arg(short = 'f', long = "file")]
-	pub ip_file: Option<std::path::PathBuf>,
+	pub ip_file: Option<String>,
 
 	/// Number of scans to run concurrently
 	#[arg(short, long, default_value_t = 1)]
--- src/probes/mod.rs
+++ src/probes/mod.rs
@@ -26,6 +26,7 @@ use crate::iterators::{ArgIterator, PortRange, ScanType};
 	** implemented as HashMap<String, HashMap<u16, ProbeStatus>>
 	** might be encapsulated in a struct for easier access
 */
+#[derive(Debug)]
 pub struct ProbeBuilder {
 	hosts: Peekable<IntoIter<Ipv4Addr>>,
 	ports: Peekable<ArgIterator<PortRange>>,
@@ -39,18 +40,21 @@ impl ProbeBuilder {
 	pub fn new(mut options: cli::Args) -> Result<Self> {
 		/* Load ip addresses from file */
 		if let Some(path) = &options.ip_file {
-			let file = std::fs::File::open(path).with_context(|| format!("Cannot read \"{}\"", path.display()))?;
+			let file = std::fs::File::open(path.trim()).with_context(|| format!("Cannot read \"{}\"", path))?;
 			let buffer = BufReader::new(file);
 
 			for line in buffer.lines() {
-				options.ip.push(line?);
+				let line = line?;
+				if line.len() > 0 {
+					options.ip.push(line);
+				}
 			}
 		}
-;
+
 		/* Turn string representation into actual IPv4 addresses */
 		let mut hosts: Vec<Ipv4Addr> = vec![];
 		for addr in options.ip.into_iter() {
-			match dns_lookup::lookup_host(&addr) {
+			match dns_lookup::lookup_host(addr.trim()) {
 				Ok(array) => {
 					match array.iter().filter(|addr| addr.is_ipv4()).next() {
 						Some(IpAddr::V4(ip)) => hosts.push(*ip),
@@ -64,6 +68,7 @@ impl ProbeBuilder {
 		if hosts.len() == 0 {
 			return Err(anyhow!("No valid target to scan"));
 		}
+		// remove duplicates ????
 
 		/* Find source IP address (needed to compute checksums) */
 		let interfaces = pnet::datalink::interfaces();
@@ -113,7 +118,7 @@ impl Iterator for ProbeBuilder {
 				host = *self.hosts.peek().unwrap();
 			} else {
 				self.ports.next();
-				port = self.ports.next().unwrap();
+				port = *self.ports.peek().unwrap();
 				self.hosts.next();
 				if let Some(h) = self.hosts.peek() {
 					host = *h;
@@ -142,6 +147,7 @@ impl Iterator for ProbeBuilder {
 				ip.set_total_length(28);
 				ip.set_next_level_protocol(IpNextHeaderProtocols::Udp);
 				udp.set_checksum(udp_checksum(&udp.to_immutable(), &self.source_addr, &host));
+				ip.set_payload(&next_protocol_header[0..8]);
 			},
 			_ => {
 				let mut tcp = MutableTcpPacket::new(next_protocol_header).unwrap();
@@ -154,34 +160,102 @@ impl Iterator for ProbeBuilder {
 				ip.set_total_length(40);
 				ip.set_next_level_protocol(IpNextHeaderProtocols::Tcp);
 				tcp.set_checksum(tcp_checksum(&tcp.to_immutable(), &self.source_addr, &host));
+				ip.set_payload(next_protocol_header);
 			}
 		};
-		ip.set_payload(next_protocol_header);
 		ip.set_checksum(checksum(&ip.to_immutable()));
 
 		Some(*packet)
 	}
 }
 
-#[test]
-fn probe_builder_hosts_iter() {}
+#[cfg(test)]
+use assert_fs::prelude::*;
+#[cfg(test)]
+use clap::Parser;
+#[cfg(test)]
+use pnet::packet::Packet;
+#[cfg(test)]
+use pnet::packet::{ipv4::Ipv4Packet, tcp::TcpPacket};
 
 #[test]
-fn probe_builder_ports_iter() {}
+fn probe_builder_hosts_iter() -> Result<(), Box<dyn std::error::Error>> {
+	const CONTENT: &str = "\t127.0.0.1\n  192.168.1.22\n\n10.13.250.188\n";
+	let tmp = assert_fs::NamedTempFile::new("ip.tmp")?;
+	tmp.write_str(CONTENT)?;
+	
+	let hosts = format!("-f {}", tmp.path().to_str().unwrap());
+	let arguments = vec![clap::crate_name!(), "-i dns.google", "-s SYN", "-p80", hosts.as_str()];
+	let builder = ProbeBuilder::new(cli::Args::try_parse_from(arguments).unwrap())?;
+	let probes: Vec<_> = builder.collect();
+	
+	let google_dns = Ipv4Packet::new(&probes[0]).unwrap().get_destination();
+	if google_dns != Ipv4Addr::new(8, 8, 8, 8) && google_dns != Ipv4Addr::new(8, 8, 4, 4){
+		assert!(false, "dns.google should resolve to 8.8.8.8 or 8.8.4.4");
+	}
+	
+	assert_eq!(Ipv4Packet::new(&probes[1]).unwrap().get_destination(), Ipv4Addr::new(127, 0, 0, 1));
+	assert_eq!(Ipv4Packet::new(&probes[2]).unwrap().get_destination(), Ipv4Addr::new(192, 168, 1, 22));
+	assert_eq!(Ipv4Packet::new(&probes[3]).unwrap().get_destination(), Ipv4Addr::new(10, 13, 250, 188));
+	Ok(())
+}
+
 
 #[test]
-fn probe_builder_scans_iter() {}
+fn probe_builder_ports_iter() -> Result<(), Box<dyn std::error::Error>> {
+	let arguments = vec![clap::crate_name!(), "-i dns.google", "-s SYN", "-p80,443,1024-1026"];
+	let builder = ProbeBuilder::new(cli::Args::try_parse_from(arguments).unwrap())?;
+	let probes: Vec<_> = builder.collect();
+	
+	assert_eq!(TcpPacket::new(Ipv4Packet::new(&probes[0]).unwrap().payload()).unwrap().get_destination(), 80);
+	assert_eq!(TcpPacket::new(Ipv4Packet::new(&probes[1]).unwrap().payload()).unwrap().get_destination(), 443);
+	assert_eq!(TcpPacket::new(Ipv4Packet::new(&probes[2]).unwrap().payload()).unwrap().get_destination(), 1024);
+	assert_eq!(TcpPacket::new(Ipv4Packet::new(&probes[3]).unwrap().payload()).unwrap().get_destination(), 1025);
+	assert_eq!(TcpPacket::new(Ipv4Packet::new(&probes[4]).unwrap().payload()).unwrap().get_destination(), 1026);
+	Ok(())
+}
+
 
 #[test]
-fn probe_builder_file_error() {}
+fn probe_builder_scans_iter() -> Result<(), Box<dyn std::error::Error>> {
+	let arguments = vec![clap::crate_name!(), "-i 127.0.0.1", "-p80"];
+	let builder = ProbeBuilder::new(cli::Args::try_parse_from(arguments).unwrap())?;
+	let probes: Vec<_> = builder.collect();
+	
+	assert_eq!(TcpPacket::new(Ipv4Packet::new(&probes[0]).unwrap().payload()).unwrap().get_flags(), u16::try_from(ScanType::SYN).unwrap());
+	assert_eq!(TcpPacket::new(Ipv4Packet::new(&probes[1]).unwrap().payload()).unwrap().get_flags(), u16::try_from(ScanType::NULL).unwrap());
+	assert_eq!(TcpPacket::new(Ipv4Packet::new(&probes[2]).unwrap().payload()).unwrap().get_flags(), u16::try_from(ScanType::ACK).unwrap());
+	assert_eq!(TcpPacket::new(Ipv4Packet::new(&probes[3]).unwrap().payload()).unwrap().get_flags(), u16::try_from(ScanType::FIN).unwrap());
+	assert_eq!(TcpPacket::new(Ipv4Packet::new(&probes[4]).unwrap().payload()).unwrap().get_flags(), u16::try_from(ScanType::XMAS).unwrap());
+	assert_eq!(Ipv4Packet::new(&probes[5]).unwrap().get_next_level_protocol(), IpNextHeaderProtocols::Udp);
+	
+	Ok(())	
+}
+
 
 #[test]
-fn probe_builder_ip_format_error() {}
+fn probe_builder_file_error() -> Result<(), Box<dyn std::error::Error>> {
+	let arguments = vec![clap::crate_name!(), "-f non_existing_file.txt"];
+	if let Ok(_) = ProbeBuilder::new(cli::Args::try_parse_from(arguments).unwrap()) {
+		assert!(false, "must complain about file existence");
+	}
+	
+	Ok(())
+}
+
 
 #[test]
-fn probe_builder_no_ip() {}
-
-#[test]
-fn probe_builder_network_interface_error() {
-	// would need to mock pnet::datalink::interfaces()
+fn probe_builder_no_ip() -> Result<(), Box<dyn std::error::Error>> {
+	const CONTENT: &str = "\t127.0.ff.1\n non.existing.domain\n\nHello, World!\n";
+	let tmp = assert_fs::NamedTempFile::new("ip.tmp")?;
+	tmp.write_str(CONTENT)?;
+	
+	let hosts = format!("-f {}", tmp.path().to_str().unwrap());
+	let arguments = vec![clap::crate_name!(), "-i foobar", hosts.as_str()];
+	
+	if let Ok(_) = ProbeBuilder::new(cli::Args::try_parse_from(arguments).unwrap()) {
+		assert!(false, "must complain about not having addresses to scan");
+	}
+	
+	Ok(())
 }
